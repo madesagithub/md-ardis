8304892427d2bde78438002ce1f33b5513073568
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2022-06-13 11:13:13 -0300

    Atualização de arquivo base

--- Fase 2/index.py
+++ Fase 2/index.py
@@ -13,7 +13,8 @@ import requests
 
 planos = []
 
-filename = 'Fase 2/FV25052205_23_22_LAYOUT.txt'
+# filename = 'Fase 2/FV25052205_23_22_LAYOUT.txt'
+filename = 'Fase 2/RELATORIO_PEÇAS.TXT'
 data = []
 with open(filename, 'r') as file_txt:
 	for line in file_txt:
@@ -22,7 +23,7 @@ with open(filename, 'r') as file_txt:
 		
 		# Remover porcentagem e asteriscos
 		line = [x.replace('%', '') for x in line]
-		line = [x.replace('*****', '100') for x in line]
+		line = [x.replace('****', '100') for x in line]
 
 		# Remover caracteres desnecessários
 		line = [i for i in line if i != '' and i != '\n']
@@ -32,14 +33,16 @@ with open(filename, 'r') as file_txt:
 		
 		data.append(line)
 
+	# Dados do projeto
 	# Dados do cabeçalho do arquivo
 	cabecalho = {
 		'nome_projeto': data[0][0].upper(),
-		'maquina': data[0][1].split(':')[1].strip().title(),
+		'maquina': data[0][1].strip().title(),
 		# 'tempo_maquina': data[0][2],
 		'data_processamento': data[0][2],
 		# 'hora_processamento': data[0][2],
 		'usuario': data[0][3].title(),
+		# 'fabrica': ,
 	}
 
 	# Tamanhos das arrays de dados
@@ -49,20 +52,20 @@ with open(filename, 'r') as file_txt:
 
 	for line in data[1:-1]:
 		
-		# Cabeçalhos = max(lens)
-		# Projeto
-		if len(line) == max(lens):
+		# Cabeçalhos = min(lens)
+		# Plano
+		if len(line) == min(lens):
 
 			# Arquivo de entrada
-			# line[0]	= No.				= numero_plano
-			# line[1] 	= %					= aproveitamento
-			# line[2] 	= Desc. Material	= descricao_material
-			# line[3] 	= Código			= codigo_material
-			# line[4]	= Comp.				= comprimento_material
-			# line[5]	= Larg.				= largura_material
-			# line[6]	= Qtd.				= quantidade_material
-			# line[7]	= m²				= area_material
-			# line[8]	= tempo				= tempo
+			# line[0]	= No.					= numero_plano
+			# line[1] 	= %						= aproveitamento
+			# line[2] 	= Desc. Material		= descricao_material
+			# line[3] 	= Código				= codigo_material
+			# line[4]	= Comp.					= comprimento_material
+			# line[5]	= Larg.					= largura_material
+			# line[6]	= Qtd.					= quantidade_material
+			# line[7]	= tempo de processo		= tempo_processo
+			# line[8]	= m²					= area_material
 
 			plano = {
 				'numero_plano': int(line[0]),
@@ -72,23 +75,24 @@ with open(filename, 'r') as file_txt:
 				'comprimento_material': int(line[4]),
 				'largura_material': int(line[5]),
 				'quantidade_material': int(line[6]),
-				# 'area_material': int(line[6])
-				# 'tempo': int(line[6])
+				'tempo_processo': line[7],
+				'area_material': float(line[8])
 			}
 
 		else:
-			# Plano = min(lens)
+			# Peça = max(lens)
 
 			# Arquivo de entrada
-			# line[0]	= #				= numero_peca
-			# line[1] 	= Código		= codigo_peca
-			# line[2] 	= Descrição		= descricao_peca
-			# line[3]	= Comp.			= comprimento_peca
-			# line[4]	= Larg.			= largura_peca
-			# line[5] 	= Qtd.			= quantidade_peca
-			# line[6]	= Ordem			= ordem
-			# line[6]	= Data			= data_embalagem
-			# line[7]	= Produzido		= produzido
+			# line[0]	= #					= numero_peca
+			# line[1] 	= Código			= codigo_peca
+			# line[2] 	= Descrição			= descricao_peca
+			# line[3]	= Comp.				= comprimento_peca
+			# line[4]	= Larg.				= largura_peca
+			# line[5] 	= Qtd.				= quantidade_peca
+			# line[6]	= Ordem				= ordem
+			# line[7]	= Data				= data_embalagem
+			# line[8]	= Produzido			= produzido
+			# line[9]	= Código cadastro	= cod_cadastro
 
 			peca = {
 				'numero_peca': int(line[0]),
@@ -97,9 +101,10 @@ with open(filename, 'r') as file_txt:
 				'comprimento_peca': int(line[3]),
 				'largura_peca': int(line[4]),
 				'quantidade_peca': int(line[5]),
-				'ordem': int(line[6]),
+				'ordem': int(line[6]) if line[6] else line[6],
 				'data_embalagem': datetime.strptime(line[7], '%d/%m/%y').strftime('%d/%m/%Y'),
-				'produzido': float(line[8])
+				'produzido': float(line[8]),
+				'cod_cadastro': int(line[9]) if line[9] else line[9]
 			}
 
 			insert = {}
@@ -110,23 +115,63 @@ with open(filename, 'r') as file_txt:
 			planos.append(insert)
 
 
+# --------------------------------------------------
 # Baixa na TOTVS
+# Movimentação (deposito -> fabrica)
 for plano in planos:
+	# MD-PROT - Sinaliza que a operação será feita na base de PROTOTIPO
+	sistema = 'MD-PROT'
+
+	# Chave da operação, necessária para o servidor aceitar a conexão
+	# codChave=128964ard
+	cod_chave = '128964ard'
+
+	# Item a ser movimentado estoque
+	item = str(plano['codigo_material'])
+
+	# Depósito de origem
+	dep_origem = 'ALM'
+
+	# Local de origem
+	fabrica = 'FB'
+	if fabrica == 'FB':
+		loc_origem = 'ALMB-A'
+	elif fabrica == 'FV':
+		loc_origem = 'ALMV-A'
+
+	# dep_dest à deposito destino
+	dep_destino = 'FAB'
+	# Local destino
+	loc_destino = ''
+
+	# Quantidade deve ser na unidade de medida cadastrada no sistema
 	quantidade = (plano['comprimento_peca'] / 1000) * (plano['largura_peca'] / 1000) * plano['quantidade_peca']
-	api_totvs = 'http://192.168.0.26:8888/scripts/cgiip.exe/WService=MD/rsapi/rsapi015web?codChave=128964ard&Item=' + str(plano['codigo_material'])  + '&dep_orig=ALM&loc_orig=ALMB-A&dep_dest=FAB&loc_dest=&quantidade=' + str(quantidade) + '&codEmitente=138449'
+	quantidade = str(quantidade)
+
+	# Emitente ARDIS
+	# codEmitente=138449
+	cod_emitente = str(138449)
+
+	# ----------
+
+	# Api para comunicação com o TOTVS
+	api_totvs = 'http://192.168.0.26:8888/scripts/cgiip.exe/WService=MD-PROT/rsapi/rsapi015web?codChave='+ cod_chave +'&Item='+ item +'&dep_orig='+ dep_origem +'&loc_orig='+ loc_origem +'&dep_dest='+ dep_destino +'&loc_dest='+ loc_destino +'&quantidade='+ quantidade +'&codEmitente=' + cod_emitente
+	print(api_totvs)
 	
 	# response = requests.get(api)
 	# response.content
 	# print(api_totvs)
 
 
+# --------------------------------------------------
+# Cadastrar no sistema de controle de chapas (php)
 
 # Converter dicionario em json
 planos = json.dumps(planos, indent = 4)
+print(planos)
 
-# Cadastrar no sistema de controle de chapas (php)
 api_endpoint = 'http://localhost/md-ardis/Fase%203/public/api/projeto'
-api_endpoint = 'http://localhost:8080/md-ardis/Fase%203/public/api/projeto'
+# api_endpoint = 'http://localhost:8080/md-ardis/Fase%203/public/api/projeto'
 api_headers = {
 	'Content-Type': 'application/json',
 }
@@ -136,9 +181,3 @@ post = requests.post(url=api_endpoint, headers=api_headers, json=planos)
 print(post.content)
 print(post.status_code)
 
-# for ordem in ordens:
-# 	print('{')
-# 	for line in ordem:
-# 		print('\t', line, ':', ordem[line])
-# 	print('}')
-# print(ordens)
---
+++ README.md
@@ -0,0 +1,16 @@
+# MD-Furação
+
+## Fase 1
+- [x] Gerar modelo de relatório de ordem de corte no Ardis.
+- [x] Criar ferramenta para gerar arquivo atualizado do estoque de chapas com base nos dados do sistema TOTVS. [python]
+- [ ] Integrar o Ardis com a ferramenta criada.
+
+## Fase 2
+- [ ] Após os planos de corte serem processados, movimentar chapas (depósito -> fabrica) [python]
+- [x] Enviar dados para API de controle na web. [python]
+- [ ] Salvar dados localmente para prevenir eventuais falhas na rede.
+
+## Fase 3
+- [x] API receber dados de projetos do Ardis. [php]
+- [ ] Sistema para usuário registrar o corte de chapas. [php] 
+- [ ] Após o usuário dar baixa para finalizar processo, movimentar chapas (ardis -> fábrica) (retorno de peças: fabrica -> deposito)
